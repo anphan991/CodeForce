4a698ab5bfd9fbf220dfc92094618b970dffc419
anphan <an0915129080@gmail.com>
2025-11-15 15:38:18 +0700

    update

---
+++ StonesOnTheTable.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	string s;
+	int n;
+	cin>>n;
+	cin>>s;
+	int count=0;
+	
+	for(int i=0; i<n-1; i++){
+		if(s[i] == s[i+1]){
+			count+=1;
+		}
+	}
+	
+	cout<<count;
+}
